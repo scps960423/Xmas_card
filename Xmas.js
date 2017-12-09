120397877601aa92b68318e418e0fd058dc1fe99
林韻如 <scps960423@gmail.com>
2017-12-09 12:49:02 +0800

    互動式賀卡

--- Xmas.js
+++ Xmas.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
   var $Xms = $(".XmasCard"),
       $but =$(".but");
-
-  $but.click(function(evt) {
+      
+    $but.click(function(evt) {
     $Xms.css("display","block")
   })
-  
 })
