91a2ff595ad7fd1c1fbc7ba5572d18037f965dae
林韻如 <scps960423@gmail.com>
2017-12-11 13:27:12 +0800

    更新動畫方法，新增改變圖片及聖誕區塊display的語法

--- Xmas.js
+++ Xmas.js
@@ -1,65 +1,56 @@
 $(document).ready(function () {
   var $Xms = $(".XmasCard"),
-    $but = $(".but");
+      $but = $(".but"),
+      $img = $(".img").find("img"),
+      $src = $img.attr("src");
 
-  $but.click(function (evt) {
+
+  if ($src =="./images/按讚4-3.png")
+  {
+    $img.css("display", "none");
     $Xms.css("display", "block")
-  })
 
-  var $li = $(" .XmasCard .day");
-  var $flag = false;
-
-
-  for (let i = 0; i < 24; i++) {
-    $li.eq(i).click(function (evt) {
-      if ($flag == false) {
-        $flag = true;
-        $li.eq(i).css({
-          "color": "transparent",
-          "border-color": "#565656",
-          "-webkit-transform": "rotateY(180deg)",
-          "-moz-transform": "rotateY(180deg)",
-          "transform": "rotateY(180deg)",
-          "background": "#565656 url('./images/gift/gift_" + (i + 1) + ".png') 3px no-repeat",
-        })
-      } else {
-        $flag = false
-        $li.eq(i).css({
-          "color": "#ffffff",
-          "border-color": "#ffffff",
-          "-webkit-transform": "rotateY(0deg)",
-          "-moz-transform": "rotateY(0deg)",
-          "transform": "rotateY(0deg)",
-          "background": "transparent",
-        })
-      }
-    })
   }
-  $li.eq(24).click(function (evt) {
-    if ($flag == false) {
-       $flag = true;
-      $li.eq(24).animate({
-        top: "35px",
-        left: "150px",
-        width: "500px",
-        height: "300px",
-      }, 0)
-      $li.eq(24).css({
-        "background": "#fff url('./images/gift/gift_25.png') 3px no-repeat",
-      })
-    }
-    else{
-       $flag = false;
-          $li.eq(24).animate({
-            top: "292px",
-            left: "370px",
-            width: "55px",
-            height: "55px",
-          }, 0)
-          $li.eq(24).css({
-            "background": "transparent",
-          })
-    }
+    
 
-  })
+  // $but.click(function (evt) {
+  //   $Xms.css("display", "block")
+  // })
+
+  var $li = $(" .XmasCard .day"),
+      $day_rotateY = "day_rotateY";
+  // var $flag = false;
+  $li.click(function (evt) {
+    $(this).toggleClass($day_rotateY);
+  });
+
+  // $li.eq(24).click(function (evt) {
+  //    $(this).toggleClass($day_rotateY);
+
+  //   if ($flag == false) {
+  //      $flag = true;
+  //     $li.eq(24).animate({
+  //       top: "35px",
+  //       left: "150px",
+  //       width: "500px",
+  //       height: "300px",
+  //     }, 0)
+  //     $li.eq(24).css({
+  //       "background": "#fff url('./images/gift/gift_25.png') 3px no-repeat",
+  //     })
+  //   }
+  //   else{
+  //      $flag = false;
+  //         $li.eq(24).animate({
+  //           top: "292px",
+  //           left: "370px",
+  //           width: "55px",
+  //           height: "55px",
+  //         }, 0)
+  //         $li.eq(24).css({
+  //           "background": "transparent",
+  //         })
+  //   }
+
+  // })
 })
