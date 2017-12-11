f3be320a041e2dbf3364a393ff05221c2328b50d
林韻如 <scps960423@gmail.com>
2017-12-11 10:01:12 +0800

    點擊翻牌，在點擊蓋牌

--- Xmas.js
+++ Xmas.js
@@ -1,8 +1,41 @@
 $(document).ready(function () {
   var $Xms = $(".XmasCard"),
-      $but =$(".but");
-      
-    $but.click(function(evt) {
-    $Xms.css("display","block")
+    $but = $(".but");
+
+  $but.click(function (evt) {
+    $Xms.css("display", "block")
   })
+
+  var $li = $(" .XmasCard .day");
+  var $flag = false;
+  for (let i = 0; i < 25; i++) {
+    $li.eq(i).click(function (evt) {
+      if ($flag == false) {
+        $flag = true;
+        $li.eq(i).css({
+          "color": "transparent",
+          "border-color": "#565656",
+          "-webkit-transform": "rotateY(180deg)",
+          "-moz-transform": "rotateY(180deg)",
+          "transform": "rotateY(180deg)",
+          "background": "#565656 url('./images/gift/gift_" + (i + 1) + ".png') 3px no-repeat",
+        })
+      }
+      else{
+        $flag = false
+          $li.eq(i).css({
+            "color": "#ffffff",
+            "border-color": "#ffffff",
+            "-webkit-transform": "rotateY(0deg)",
+            "-moz-transform": "rotateY(0deg)",
+            "transform": "rotateY(0deg)",
+            "background": "transparent",
+          })
+      }
+
+    })
+
+
+  }
+
 })
