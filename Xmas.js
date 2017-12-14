690498035b951b0b90e1ff7e8e3600b5c758b6e5
林韻如 <scps960423@gmail.com>
2017-12-14 14:10:10 +0800

    顯示圖片的的js

--- Xmas.js
+++ Xmas.js
@@ -1,19 +1,4 @@
 $(document).ready(function () {
-  var $Xms_1 = $(".XmasCard_1"),
-    $img = $(".card_main").find("img"),
-    _src = $img.attr("src");
-
-  console.log(_src);
-  if (_src == "./Images/card/sort04/functional_012.gif") {
-    $img.css("display", "none");
-    $Xms_1.css("display", "block")
-
-  }
-  var $li = $(" .XmasCard_1 .day"),
-    $day_rotateY = "day_rotateY";
-  $li.click(function (evt) {
-    $(this).toggleClass($day_rotateY);
-  });
 
   var $Xms_2 = $(".XmasCard_2"),
     $gift_1 = $(".gift_1"),
@@ -39,11 +24,56 @@ $(document).ready(function () {
     $doll_st = "doll_st",
     $tsmc_open = "tsmc_bor_open ",
     $tsmc_close = "tsmc_bor_close"
-    $tsmc_st = "tsmc_bor_st"
-  ;
+  $tsmc_st = "tsmc_bor_st";
+
+  var $Xms_1 = $(".XmasCard_1"),
+    $img = $(".card_main").find("img");
+
+    // $img = [img1, img2, img3, img4]
+
+    // $img.each(function(i, n){
+    //   $img.eq(i) = $(n)
+    // })
+
+    $img.each(function(i, n){
+      var $this = $(this),
+        _src = $this.attr("src");
+
+        if (_src.match('functional_012.gif')) {
+          $this.css("display", "none");
+          $Xms_1.css("display", "block")
+        }
+
+        if (_src.match('functional_013.gif')) {
+          $this.css("display", "none");
+          $Xms_2.css("display", "block")
+        }
+    });
+    
+  var $li = $(" .XmasCard_1 .day"),
+    $day_rotateY = "day_rotateY";
+  $li.click(function (evt) {
+    $(this).toggleClass($day_rotateY);
+  });
+
+
+
+  var $Xms_1 = $(".XmasCard_1"),
+    $img = $(".card_main").find("img"),
+    _src = $img.attr("src");
+
+
+  var $sled = $(".XmasCard_2 .sled"),
+      $Xmas2_text = $(".Xmas2_text"),
+      $sled_run = "sled_run";
+$Xmas2_text.click(function () {
+    $sled.toggleClass("sled_run");
+  })
+    
 
   var flag1 = false,
-    flag2 = false;
+      flag2 = false;
+
   $gift_1.click(function () {
     if (flag1 == false) {
       $cover_1.addClass($gift_cover_open).removeClass($gift_cover_close + " " + $cover_st);
@@ -60,7 +90,6 @@ $(document).ready(function () {
       
       flag1 = false;
     }
-
   })
   $gift_2.click(function () {
     if (flag2 == false) {
@@ -76,5 +105,6 @@ $(document).ready(function () {
     }
 
   })
+
 
 })
